3a57be073d34a11278de1f758bb28fe5e07eb4dc
Jan Ryklikas <jan_ryklikas@ymail.com>
2024-06-09 00:00:51 +0200

    refactor: remove helper

--- src/helper.cpp
+++ src/helper.cpp
@@ -5,14 +5,11 @@ MyBMS::shared_bms_data_t *MyHelper::_myBMSData;
 void MyHelper::initHelper(MyBMS::shared_bms_data_t *myBMSData) {
     initSERIAL();
     initI2C();
-    pinMode(13, INPUT_PULLUP);
+    
 }
 
 void MyHelper::initSERIAL() {
-    Serial.begin(115200);
-    Serial.println("INIT SERIAL - START");
-    delay(3000);
-    Serial.println("INIT SERIAL - DONE");
+
 }
 
 void MyHelper::initI2C() {
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
 #define FEATURE_BT
 
 #include <Arduino.h>
+#include <Wire.h>
 
-#include "helper.h"
 #include "logging.h"
 #include "display.h"
 #include "bluetooth.h"
 #include "bms.h"
 
+#define I2C_SDA 32
+#define I2C_SCL 33
+
+#define REED_PIN 13
+
 MyBMS::shared_bms_data_t myBMSData;
 
 void setup() {
-    MyHelper::initHelper(&myBMSData);
+    pinMode(REED_PIN, INPUT_PULLUP);
+
+    Wire.begin(I2C_SDA, I2C_SCL);
+    Serial.begin(115200);
+    delay(2000); // wait for serial
+    
     MyLogger::initLogger(&myBMSData);
     MyDisplays::initDisplays(&myBMSData);
     MyBMS::initBMS(&myBMSData);
+
     #ifdef FEATURE_BT
         MyBluetooth::initBT();
     #endif
